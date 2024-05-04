4d1d459a15b2fced9d02c3263413fd97fa653c45
EMeDesign <1158066347@protonmail.com>
2024-05-04 18:04:25 +0800

    WelcomePage: Improve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,12 @@
     </head>
     <body class="antialiased">
     <!-- TALLStackUI Dialog & Toast -->
+    @persist('dialog')
     <x-ts-dialog />
+    @endpersist
+    @persist('toast')
     <x-ts-toast />
+    @endpersist
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <livewire:welcome.navigation />
