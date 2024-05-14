77b1beb12cf9ef8e586c4c998a67c53024f987cf
EMeDesign <1158066347@protonmail.com>
2024-05-15 06:32:49 +0800

    Translation: Add Friend

--- lang/zh-CN/friend.php
+++ lang/zh-CN/friend.php
@@ -7,10 +7,6 @@ return [
     'accept' => '接受',
     'deny' => '拒绝',
 
-    // confirm
-
-    'delete-confirm' => '您确定要删除该物品吗?',
-
     // header
     'friends' => '好友',
     'friend-request' => '好友申请',
@@ -18,18 +14,9 @@ return [
     'received-friend-information' => '收到的好友申请',
     'sent-friend-information' => '已发送的好友申请',
     'send-new-friend-request' => '向其他用户发送好友申请吧.',
-    'create-your-item' => '创建属于你的物品',
-    'edit-your-item' => '编辑你的物品信息.',
 
     // hint
-    'insert-name' => '输入物品名称',
-    'insert-location' => '输入物品地理位置',
-    'insert-description' => '输入物品简介',
-    'insert-quantity' => '输入物品数量',
     'choose-only-one' => '只能选择一个',
-    'choose-more-than-one' => '可以选择多个',
-    'select-obtained-date' => '选择购买时间',
-    'select-expired-date' => '选择购过期时间',
 
     // label
     'select-user-to-request' => '选择一名用户发送好友申请',
@@ -37,16 +24,6 @@ return [
     'accepted' => '已接受',
     'denied' => '已拒绝',
     'blocked' => '已拉黑',
-    'description' => '简介 *',
-    'quantity' => '数量 *',
-    'select-unit-bind' => '选择要绑定的单位 *',
-    'select-furniture-bind' => '选择要绑定的家具 *',
-    'select-category-bind' => '选择要绑定的分类 *',
-    'select-keyword-bind' => '选择要绑定的关键词',
-    'obtained_date' => '购买时间',
-    'expired_date' => '过期时间',
-    'private' => '私人物品',
-    'furniture' => '家具',
 
 
     // placeholder
@@ -57,7 +34,6 @@ return [
     'email-table' => '邮箱',
     'status-table' => '状态',
     'request-date' => '申请日期',
-    'no-image' => '预览图缺失',
 
     // toast
     'cannot-send-self' => '不能向自己发送好友申请!',
@@ -69,7 +45,4 @@ return [
     'unfriend-success' => '您已将 :name 移出了您的好友列表!',
     'accept-success' => '您已接受 :name 的好友申请!',
     'deny-success' => '您已拒绝 :name 的好友申请!',
-    'updated-success' => '物品更新成功!',
-    'updated-failed' => '物品更新失败!',
-    'deleted-success' => '物品删除成功!',
 ];
