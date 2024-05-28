3675079d8888f0927f6ecb7a338eb61e289f79cb
EMeDesign <1158066347@protonmail.com>
2024-05-28 17:33:37 +0800

    Api: Add missing route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\ConstructionController;
 use App\Http\Controllers\API\FurnitureController;
+use App\Http\Controllers\API\ItemController;
 use App\Http\Controllers\API\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,12 +24,20 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    // Room Resource
+    // Construction / Room / Furniture Resource
     Route::apiResources(
         resources:[
-            'rooms'     => RoomController::class,
-            'furniture' => FurnitureController::class
+            'construction' => ConstructionController::class,
+            'rooms'        => RoomController::class,
+            'furniture'    => FurnitureController::class
         ],
         options: ['missing' => fn () => response()->json('Error 404: No Resource Found!', 404)]
+    );
+
+    // Item Resource
+    Route::apiResource(
+        name: 'items',
+        controller: ItemController::class,
+        options: ['only' => ['index', 'show', 'destroy']],
     );
 });
